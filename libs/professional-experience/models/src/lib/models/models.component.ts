f7273475631f48e16f8030c4b40e91532e82dd83
mathausr <reselane3@hotmail.com>
2024-11-11 07:20:01 -0300

    add lib professional-experience

---
+++ libs/professional-experience/models/src/lib/models/models.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-models',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './models.component.html',
+  styleUrl: './models.component.css',
+})
+export class ModelsComponent {}
---
+++ libs/professional-experience/routes/src/lib/routes/routes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-routes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './routes.component.html',
+  styleUrl: './routes.component.css',
+})
+export class RoutesComponent {}
